8570c6a224abea80251f7161432ad7357a2443ad
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-02-08 22:19:05 +0300

    commit tree 51575577a2c5c3f5036400994ca032517b838960

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|!/).filter {|sentence| !sentence.empty?}.size
   end
 end
+ "Hi,my name is remmy.".sentence?
+  "vipi kaka?".question?
+  "Allahu akbar!".exclamation?
+  "returns the number of sentences".count_sentences
+"This is a string! It has three sentences. Right?".count_sentences
